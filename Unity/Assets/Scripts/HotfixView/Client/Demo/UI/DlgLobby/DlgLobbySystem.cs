7c77e94caa9eec27891fcc63a9500908e9f17106
mnfengqing <450282550@qq.com>
2024-06-16 17:00:28 +0800

    去除UIEventComponent单例，采用Scene传参调用，更符合ET写法

--- Unity/Assets/Scripts/HotfixView/Client/Demo/UI/DlgLobby/DlgLobbySystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/DlgLobby/DlgLobbySystem.cs
@@ -12,7 +12,7 @@ namespace ET.Client
 
 		public static void RegisterUIEvent(this DlgLobby self)
 		{
-			self.View.E_EnterMapButton.AddListenerAsync(self.EnterMap);
+			self.View.E_EnterMapButton.AddListenerAsync(self.Root(),self.EnterMap);
 		}
 
 		public static void ShowWindow(this DlgLobby self, Entity contextData = null)
